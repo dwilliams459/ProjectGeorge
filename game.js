6d19218df09875582a5f8af70ed45d20b2f5c0fb
David Williams <dwilliams459@gmail.com>
2022-07-28 18:24:48 -0400

    Hopefully Nothing Breaks

--- game.js
+++ game.js
@@ -19,3 +19,60 @@ var status = "start";  // or use 'start', 'saidHello', 'done', or 1, 2, 3....
             term.echo("Should I be worried?");
         },
     });
+
+    /* can delete from line ~105 to ~143 (the following section) without issue
+            this.read("Enter name here:", function(result) 
+            {
+                term.echo("Hello " + result);
+
+                term.read("What do you want to do now?", function(result2) {
+                    term.echo("You said" + result2);
+                })
+            });
+            //var name = prompt("And you are?");
+
+            if (status === 'start' && command === 'good') {
+                // Demo
+            } else if (command == 'bad') {
+                // Demo
+            }
+            test()
+        
+
+            /* else {
+                 if (command !== '') {
+                     try {
+                         var result = window.eval(command);
+                         if (result !== undefined) {
+                             this.echo(new String(result));
+                         }
+                     } catch (e) {
+                         this.error(new String(e));
+                     }
+                 } else {
+                     this.echo('');
+                 }
+                }
+                */ 
+        
+
+                function test() {
+                    alert("My Test Function");
+                }
+            
+                function timer() {
+                    sleep(1000);
+                    var a = a + 10;
+                }
+            
+                function sleep(milliseconds) {
+                 const date = Date.now();
+                 let currentDate = null;
+                 do {
+                   currentDate = Date.now();
+                 } while (currentDate - date < milliseconds);
+            }
+            
+            // console.log("Hello");
+            // sleep(2000);
+            // console.log("World!");
