56908037c05fb8a5640b129e230f8ed15fed2f14
David Williams <dwilliams459@gmail.com>
2022-09-05 22:40:37 -0400

    Finish POC

--- game.js
+++ game.js
@@ -1,64 +1,82 @@
-    let pause = 0
+// Use this veriable to keep track of where a user is in the flow.
+// Value could be anything such as 'start', 'saidHello', 'done', 1, 2, 3...  etc.
+var userStatus = "start";   // <<<<<<<<<<
+var terminal;
 
-    //var __EVAL = (s) => eval(`void (__EVAL = ${__EVAL}); ${s}`);
-
-    let term;
-    jQuery(function($, undefined) {
-        $('#term-box').terminal(function(command) {
-            cat: {
-                this.echo("my cat");
+jQuery(function ($, undefined) {
+    $('#term-box').terminal(function (command) {
+        // cat: {
+        //     this.echo("my cat");
+        // }
+        if (command !== '') {
+            try {
+                var result = George(command);
+                // if (result !== undefined) {
+                //     this.echo(new String(result));
+                // }
+            } catch (e) {
+                this.error(new String(e));
             }
-            if (command !== '') {
-                try {
-                    var result = George(command);
-                    // if (result !== undefined) {
-                    //     this.echo(new String(result));
-                    // }
-                } catch(e) {
-                    this.error(new String(e));
-                }
-            } else {
-               this.echo('');
-               this.scroll_to_bottom();
-            }
-        }, {
-            greetings: 'Hello there!',
-            name: 'js_demo',
-            height: 400,
-            width: 600,
-            prompt: '> '
-        });
-        term = $('#term-box').terminal();
+        } else {
+            this.echo('');
+            this.scroll_to_bottom();
+        }
+    }, {
+        greetings: 'Hello there!',
+        name: 'js_demo',
+        height: 400,
+        width: 600,
+        prompt: '> '
     });
+    terminal = $('#term-box').terminal();
+    userStatus = "start";
+    startGeorge();
+});
 
-    // Not really working.  Look at https://www.sitepoint.com/delay-sleep-pause-wait/
-    function sleep(milliseconds) {
-        const date = Date.now();
-        let currentDate = null;
-        do {
-          currentDate = Date.now();
-        } while (currentDate - date < milliseconds);
+// Not really working.  Look at https://www.sitepoint.com/delay-sleep-pause-wait/
+function sleepOld(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// From here, reference this to look for terminal commands (Instance Methods section): https://terminal.jcubic.pl/api_reference.php#instance_methods
+
+// Start of application
+function startGeorge() {
+    terminal.echo("Welcome");
+    terminal.echo("test 2");
+
+    terminal.pause();
+    sleep(2000).then(() => {
+        terminal.echo("test 3");
+        sleep(2000).then(() => {
+            terminal.echo("test 4");
+            terminal.resume();
+        })
+    });
+}
+
+function George(command) {
+    command = command.toLowerCase();
+
+    if (userStatus === 'start') {
+        if (command === 'hello') {
+            terminal.echo('test 5');
+            userStatus = 'saidhello';
+        }
+    } else if (userStatus === 'saidhello') {
+        terminal.echo('test 6');
+        branch1(command);
     }
+}
 
-    // Use this veriable to keep track of where a user is in the flow.
-    // Value could be anything such as 'start', 'saidHello', 'done', 1, 2, 3...  etc.
-    let userStatus = "start";
-
-    function George(command)
-    {
-        // From here, reference this to look for terminal commands (Instance Methods section): https://terminal.jcubic.pl/api_reference.php#instance_methods
-        term.echo('You said: ' + command);
-
-        term.echo("Hello"); //console.log("Hello");
-        term.pause();
-        setTimeout(() => { nextThing() }, 5000);
-
-        command = command.toLowerCase();
-
-        term.scroll_to_bottom();
-    }
-
-    function nextThing() {
-        term.echo("Goodby.");
-        term.resume();
-    }
+function branch1(command) {
+    terminal.echo('test 7');
+}
